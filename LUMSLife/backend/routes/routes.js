c798abe7a0edd282e5a21520d389293927b4cd0f
hussainmoatsim <hussainmoatsim@gmail.com>
2023-03-27 21:33:38 +0500

    updated some files

---
+++ LUMSLife/backend/routes/routes.js
@@ -0,0 +1,12 @@
+import Router from 'express'
+
+import * as generalController from '../controllers/generalController.js'
+import * as studentController from '../controllers/studentController.js'
+import * as societyController from '../controllers/societyController.js'
+
+// creating the router
+const router = Router()
+
+//defining the functions to be called when a specific route is requested
+
+router.post('/api/general/signup', generalController.signup)
