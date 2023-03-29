963e40cd5fec7f1da3c74013a4cf125a98a00ba1
24100123 <117603639+24100123@users.noreply.github.com>
2023-03-29 06:28:45 -0700

    Add files via upload

---
+++ db_create.js
@@ -0,0 +1,82 @@
+const dotenv = require("dotenv");
+const mysql = require('mysql');
+const express = require('express');
+const bodyParser = require('body-parser')
+dotenv.config({path:".env"});
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+var connectionString = mysql.createConnection(
+    {
+        host:process.env.host,
+        user: process.env.user,
+        password:process.env.password
+    }
+);
+function createTable(CreateQuerry)
+{
+    connectionString.query(CreateQuerry,
+        (err,result)=>
+        {
+            if(err)
+            {
+                console.log("Table creation failed");
+                console.log(err);
+            }
+            else
+            {
+                console.log("Table created");
+                //console.log(result);
+            }
+        });
+}
+
+const userTable = 'CREATE TABLE IF NOT EXISTS '+process.env.database+'.User (User_id int NOT NULL AUTO_INCREMENT, User_type int, name varchar(255), email varchar(255), password_hash varchar(255), PRIMARY KEY (User_id))';
+const studentTable = 'CREATE TABLE IF NOT EXISTS '+process.env.database+'.Student (Student_id int NOT NULL AUTO_INCREMENT, User_id int NOT NULL, PRIMARY KEY (Student_id), FOREIGN KEY (User_id) REFERENCES User(User_id))';
+const societyMemberTable = 'CREATE TABLE IF NOT EXISTS '+process.env.database+'.Society_member (member_id int NOT NULL AUTO_INCREMENT, User_id int NOT NULL, membership int, PRIMARY KEY (member_id), FOREIGN KEY (User_id) REFERENCES User(User_id))';
+const adminTable = 'CREATE TABLE IF NOT EXISTS '+process.env.database+'.Admin (Admin_id int NOT NULL AUTO_INCREMENT, User_id int NOT NULL, PRIMARY KEY (Admin_id), FOREIGN KEY (User_id) REFERENCES User(User_id))';
+const societyTable = 'CREATE TABLE IF NOT EXISTS '+process.env.database+'.Society (Society_id int NOT NULL AUTO_INCREMENT, membership int, posts_id int, PRIMARY KEY (Society_id), FOREIGN KEY (posts_id) REFERENCES Posts(posts_id))';
+const postsTable = 'CREATE TABLE IF NOT EXISTS '+process.env.database+'.Posts (posts_id int NOT NULL AUTO_INCREMENT, title varchar(255), content varchar(255), user_id int, PRIMARY KEY (posts_id), FOREIGN KEY (user_id) REFERENCES User(User_id))';
+const societyMembershipTable = 'CREATE TABLE IF NOT EXISTS '+process.env.database+'.Society_membership (Society_id int NOT NULL, member_id int NOT NULL, joined bool, PRIMARY KEY (Society_id, member_id), FOREIGN KEY (Society_id) REFERENCES Society(Society_id), FOREIGN KEY (member_id) REFERENCES Society_member(member_id))';
+const interactionsTable = 'CREATE TABLE IF NOT EXISTS '+process.env.database+'.Interactions (post_id int NOT NULL, comment varchar(255), user_id int NOT NULL, liked bool, PRIMARY KEY (post_id, user_id), FOREIGN KEY (post_id) REFERENCES Posts(posts_id), FOREIGN KEY (user_id) REFERENCES User(User_id))';
+const eventsTable = 'CREATE TABLE IF NOT EXISTS '+process.env.database+'.Events (events_id int NOT NULL AUTO_INCREMENT, name varchar(255), society_id int, date date, PRIMARY KEY (events_id), FOREIGN KEY (society_id) REFERENCES Society(Society_id))';
+const eventAttendanceTable = 'CREATE TABLE IF NOT EXISTS '+process.env.database+'.Event_attendance (event_id int NOT NULL, user_id int NOT NULL, PRIMARY KEY (event_id, user_id), FOREIGN KEY (event_id) REFERENCES Events(events_id), FOREIGN KEY (user_id) REFERENCES User(User_id))';
+
+
+connectionString.connect((error)=>
+{
+    if(!error)
+    {
+        console.log("Connection has been established");
+        connectionString.query(`CREATE DATABASE IF NOT EXISTS ${process.env.database}`, (err2,result) =>
+        {
+            if(err2)
+            {
+                console.log(err2);
+            }
+            else
+            {
+                console.log("Database Created");
+                
+                createTable(userTable);
+                createTable(studentTable);
+                createTable(societyMemberTable);
+                createTable(adminTable);
+                createTable(societyTable);
+                createTable(postsTable);
+                createTable(societyMembershipTable);
+                createTable(interactionsTable);
+                createTable(eventsTable);
+                createTable(eventAttendanceTable);
+
+                connectionString.end();
+            }
+        });
+    }
+    else
+    {
+        console.log("Connection failed");
+        console.log(error);
+    }
+});
