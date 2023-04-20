8bc1d6ed6bfdfc15e7822e26fbf645bba532b41f
ShafayKashif <109824354+ShafayKashif@users.noreply.github.com>
2023-04-20 22:13:02 +0500

    Update routes.js
    changes from shafay routes to main routes

--- LUMSLife/backend/routes/routes.js
+++ LUMSLife/backend/routes/routes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const generalController = require("../controllers/generalController.js");
 const studentController = require("../controllers/studentController.js");
 const societyController = require("../controllers/societyController.js");
+const homeFeedController = require("../controllers/homeFeedController.js");
+const eventsPageController = require("../controllers/eventsPageController.js");
+const studentProfileController = require("../controllers/student_profile_backend.js");
 
 //defining the functions to be called when a specific route is requested. note: /api/general in app.use("/api/general", router) in server file is base url for any route in this file. meaning that the signup route below is actually /api/general/signup
 
@@ -30,7 +33,16 @@ router.post('/removeStudentAccount', studentController.removeStudentAccount);
 router.post('/updateStudent', studentController.updateStudent);
 router.post('/getStudentInfo', studentController.getStudentInfo);
 
-
+router.get("/homefeed", homeFeedController.getHomeFeed);
+router.post("/interact-post", studentController.interact_post);
+router.use("/events/info", eventsPageController);
+router.use("/events/attendance", eventsPageController);
+router.use("/events/create", eventsPageController);
+router.use("/events/attend", eventsPageController);
+router.get("/api/society/view_bookings", societyController.view_bookings);
+router.put("/api/society/confirm_booking", societyController.confirm_booking);
+router.post("/api/student/create_booking", studentController.bookEvent);
+router.use("/api/student/profile", studentProfileController);
 
 
 module.exports = router;
